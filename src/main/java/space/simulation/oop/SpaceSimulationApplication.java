069ff24d3c3b24f4a0b5afbd2eb2cab219a84a8c
NAntigrvity <amajski07@gmail.com>
2022-03-18 23:25:02 +0700

    Initial commit

---
+++ src/main/java/space/simulation/oop/SpaceSimulationApplication.java
@@ -0,0 +1,13 @@
+package space.simulation.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceSimulationApplication.class, args);
+    }
+
+}
